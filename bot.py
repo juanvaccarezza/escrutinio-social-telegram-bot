40545eb93d892d3174a311135cb8890336a28186
SPG <spghdev@gmail.com>
2019-07-08 13:07:55 -0300

    Implementamos que pida el contacto de la persona que le habla

--- bot.py
+++ bot.py
@@ -1,11 +1,21 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from optparse import OptionParser
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 
-def hello(bot, update):
+
+def start(bot, update):
+    reply_keyboard = [[KeyboardButton(text="Yes", request_contact=True), KeyboardButton(text="No")]
+                                ]
     update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+        'Hola {} ¿podemos usar tu número de teléfono para saber quien sos?'.format(update.message.from_user.first_name),reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    
+def text(bot, update, user_data):
+    print(update.message.text)
+    print(user_data)
 
+def contact(bot, update):
+    print(update)
 
 def parse_cmd():
     parser = OptionParser()
@@ -22,8 +32,16 @@ if __name__ == '__main__':
 
     options = parse_cmd()
     updater = Updater(options.token)
+    
 
-    updater.dispatcher.add_handler(CommandHandler('acta', hello))
+    
+    contact_handler = MessageHandler(Filters.contact, contact)
+    updater.dispatcher.add_handler(contact_handler)
 
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+
+    text_handler = MessageHandler(Filters.text, text, pass_user_data=True)
+    updater.dispatcher.add_handler(text_handler)
+    
     updater.start_polling()
     updater.idle()
